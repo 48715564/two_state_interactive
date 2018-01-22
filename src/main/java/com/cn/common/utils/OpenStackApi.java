d5ed9462c1ab2d2552fb6cb9e189a7f87b0e051b
zhoubo <zb@zallhy.com>
2018-01-22 18:55:14 +0800

    11212

--- src/main/java/com/cn/common/utils/OpenStackApi.java
+++ src/main/java/com/cn/common/utils/OpenStackApi.java
@@ -20,37 +20,20 @@ public class OpenStackApi {
     private String password;
     @Value("${OpenStack.identity3Url}")
     private String apiHost;
-    @Autowired
-    private Provider provider;
-    private Cache<String,OSClientV3> fifoCache;
 
 
     public OSClientV3 getAuthenticateUnscoped(){
-        if(fifoCache==null||fifoCache.get("unscoped")==null) {
-            OSClientV3 osClientV3 = OSClientV3Factory.authenticateUnscoped(apiHost, userId, password);
-            Long timeOut = osClientV3.getToken().getExpires().getTime()-provider.now().getTime();
-            fifoCache = CacheUtil.newTimedCache(timeOut-10000);
-            fifoCache.put("unscoped",osClientV3);
-            return osClientV3;
-        }else {
-            OSClientV3 osClientV3 = fifoCache.get("unscoped");
-            return osClientV3;
-        }
+        return OSClientV3Factory.authenticateUnscoped(apiHost, userId, password);
     }
 
     public OSClientV3 getAuthenticateWithProjectScope(String projectId){
         OSClientV3 authenticateUnscoped = getAuthenticateUnscoped();
-        if(fifoCache.get(projectId)==null) {
             for (Project project : authenticateUnscoped.identity().projects().list()) {
                 if (projectId.equals(project.getId())) {
                     OSClientV3 osClientV3 = OSClientV3Factory.authenticateWithProjectScope(apiHost, authenticateUnscoped.getToken().getUser().getName(), password, project.getDomainId(), project.getId());
-                    fifoCache.put(projectId, osClientV3);
                     return osClientV3;
                 }
-            }
-            throw new ResponseException("没有找到对应的project", org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-        }else{
-            return fifoCache.get(projectId);
         }
+        throw new ResponseException("没有找到对应的project", org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
