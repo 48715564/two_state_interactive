c8c9b0bd0ec668f4274ba4cd05a241e0250be2c2
zhoubo <zb@zallhy.com>
2018-01-24 18:22:09 +0800

    11212

--- src/main/java/com/cn/common/utils/OSClientV3Factory.java
+++ src/main/java/com/cn/common/utils/OSClientV3Factory.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class OSClientV3Factory {
 
     public static OSClientV3 authenticateUnscoped(OverridableEndpointURLResolver endpointResolver,String endpoint,String user_id, String secret){
-        OSClientV3 os = OSFactory.builderV3()
+        OSClientV3 os = OSFactory.builderV3().withConfig(Config.newConfig().withEndpointURLResolver(endpointResolver))
                 .endpoint(endpoint)
                 .credentials(user_id, secret)
                 .authenticate();
@@ -26,7 +26,7 @@ public class OSClientV3Factory {
     }
 
     public static OSClientV3 authenticateWithProjectScope(OverridableEndpointURLResolver endpointResolver,String endpoint,String user_id, String secret,String user_domain_id,String project_id){
-        OSClientV3 os = OSFactory.builderV3()
+        OSClientV3 os = OSFactory.builderV3().withConfig(Config.newConfig().withEndpointURLResolver(endpointResolver))
                 .endpoint(endpoint)
                 .credentials(user_id, secret, Identifier.byId(user_domain_id))
                 .scopeToProject(Identifier.byId(project_id))
@@ -45,7 +45,7 @@ public class OSClientV3Factory {
     }
 
     public static OSClientV3 authenticateWithAToken(OverridableEndpointURLResolver endpointResolver,String endpoint,String token_id, String project_id){
-        OSClientV3 os = OSFactory.builderV3()
+        OSClientV3 os = OSFactory.builderV3().withConfig(Config.newConfig().withEndpointURLResolver(endpointResolver))
                 .endpoint(endpoint)
                 .token(token_id)
                 .scopeToProject(Identifier.byId(project_id))
@@ -94,8 +94,8 @@ public class OSClientV3Factory {
         // 添加各个服务的endpoint绑定
         OverridableEndpointURLResolver endpointResolver = new OverridableEndpointURLResolver();
 
-        endpointResolver.addOverrideEndpoint(ServiceType.COMPUTE,
-                "https://vbs.example.com/v2/%(project_id)s");
+//        endpointResolver.addOverrideEndpoint(ServiceType.COMPUTE,
+//                "http://192.168.0.148:8774/v2.1/cc46d392edd740f0859bedc75e07bcff");
 //
 //        endpointResolver.addOverrideEndpoint(ServiceType.IMAGE,
 //                "https://as.example.com/autoscaling-api/v1/%(project_id)s");
@@ -106,17 +106,17 @@ public class OSClientV3Factory {
 //                .credentials("16b24e4f62fd4a70888732b12ace1bda", "FQiYa5tlBDJu4C5kqvQPEu4Z5nvfeSYXA1aMltl1", Identifier.byName("default"))
 //                .scopeToProject(Identifier.byName("admin"))
 //                .authenticate();
-        OSClientV3 os = OSFactory.builderV3().withConfig(Config.newConfig().withEndpointURLResolver(endpointResolver))
-                .endpoint("http://identity.daocloud.cc/v3")
-                .credentials("admin", "FQiYa5tlBDJu4C5kqvQPEu4Z5nvfeSYXA1aMltl1", Identifier.byId("default"))
-                .scopeToProject(Identifier.byId("ca2a6d04c5f94f0e8c696f22b1ac5819"))
-                .authenticate();
+//        OSClientV3 os = OSFactory.builderV3().withConfig(Config.newConfig().withEndpointURLResolver(endpointResolver))
+//                .endpoint("http://identity.daocloud.cc/v3")
+//                .credentials("admin", "FQiYa5tlBDJu4C5kqvQPEu4Z5nvfeSYXA1aMltl1", Identifier.byId("default"))
+//                .scopeToProject(Identifier.byId("ca2a6d04c5f94f0e8c696f22b1ac5819"))
+//                .authenticate();
 
 //        List<? extends Endpoint> endpointList = os.identity().serviceEndpoints().listEndpoints();
 //        endpointList.forEach(item->{
 //
 //        });
-//        OSClientV3 os = authenticateUnscoped("http://identity.daocloud.cc/v3","16b24e4f62fd4a70888732b12ace1bda","FQiYa5tlBDJu4C5kqvQPEu4Z5nvfeSYXA1aMltl1");
-        System.out.println(endpointResolver.getOverrides().isEmpty());
+        OSClientV3 os = authenticateUnscoped(endpointResolver,"http://my-openstack:5000/v3","81210da392d94ad982cfcef826b5acd2","7879f017c2b04fa7");
+        System.out.println(os.compute().hypervisors().list().size());
     }
 }
